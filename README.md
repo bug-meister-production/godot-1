3ed18a679e54219ce3ca0f86908e511c95823e3f
sirvadim <sir.vadim2013@yandex.ru>
2019-01-24 23:46:18 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# simple scene in godot #1
---
+++ ball.gd
@@ -0,0 +1,22 @@
+extends RigidBody2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+#var speed = Vector2(rand_range(-4, 4),rand_range(-4, 4))
+
+func _ready():
+#	print(speed)
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+# cmd + K comment
+func _process(delta):
+#	if(position.x < 0 || position.x > get_viewport_rect().size.x):
+#		speed.x *= -1
+#	if(position.y < 0 || position.y > get_viewport_rect().size.y):
+#		speed.y *= -1
+	# Called every frame. Delta is time since last frame.
+	# Update game logic here.
+#	move_and_slide(speed)
+	pass
